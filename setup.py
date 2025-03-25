a9e03706cf4578a845b6ef1f030b80c2b71b71e5
EDITOR <aeditor.rc@gmail.com>
2025-03-25 13:47:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rplusrbot',
+    version='0.1.0',
+    description='Telegram bot for rating and RnD',
+    author='EDITORrc',
+    author_email='aeditor.rc@gmail.com',
+    install_requires=['python-telegram-bot==20.7'],
+    python_requires='>=3.10',
+)
